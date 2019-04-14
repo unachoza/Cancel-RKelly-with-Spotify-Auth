72fc5cc07553cd74a657fb2b21e18b2a21340904
Arianna Choza <unachoza@gmail.com>
2019-04-13 23:21:43 -0400

    more boiler plate

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
-import React from 'react-dom'
-import ReactDom from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import App from './Components/App'
 
-ReactDOM.render(<App/>, document.querySelector("#root"))
+ReactDOM.render(<App />, document.querySelector('#root'))
