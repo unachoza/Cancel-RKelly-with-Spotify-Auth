4773a97660d1516cef0775480129e81e493a85ad
Arianna Choza <unachoza@gmail.com>
2019-04-14 07:58:23 -0400

    minimal styling

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import '../App.css'
 
 const App = () => {
-    return <div>Log in to Spotify</div>
+    return (
+        <div className="home">
+            <h1>Log in to Spotify</h1>
+            <a href="http://localhost:8888">
+            <button>Login</button>
+            </a>
+        </div>
+    )
 }
 
 export default App
