a447966bb3193103f5b481925ba593a2dd950297
Arianna Choza <unachoza@gmail.com>
2019-04-13 22:29:03 -0400

    boiler plate react

---
+++ client/src/Components/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const App = () => {
+    return <div>Log in to Spotify</div>
+}
+
+export default App
---
+++ client/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react-dom'
+import ReactDom from 'react-dom'
+import App from './Components/App'
+
+ReactDOM.render(<App/>, document.querySelector("#root"))
