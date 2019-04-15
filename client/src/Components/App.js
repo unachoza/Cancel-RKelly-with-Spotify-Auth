16e420d172a3f2701268c11ecbb7fcc4387bd009
Arianna Choza <unachoza@gmail.com>
2019-04-15 04:25:23 -0400

    initializing state

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,15 +1,45 @@
-import React from 'react'
+import React, {Component} from 'react'
 import '../App.css'
 
-const App = () => {
-    return (
-        <div className="home">
+class App extends Component {
+    constructor() {
+        super()
+        const params = this.getHashParams()
+        this.state = {
+            loggedIn: params.access_token ? true : false,
+            nowPlaying = {
+                name: "Not Checked", 
+                image: ''
+            }
+        }
+    }
+    
+
+    // getHashParams() {
+    //     console.log('something')
+    //       var hashParams = {};
+    //       var e, r = /([^&;=]+)=?([^&;]*)/g,
+    //           q = window.location.hash.substring(1);
+    //       while ( e = r.exec(q)) {
+    //          hashParams[e[1]] = decodeURIComponent(e[2]);
+    //       }
+    //       return hashParams;
+    // }
+        
+    render() {
+        return (
+            <div className="home">
+                <h1>Arianna's App</h1>
             <h1>Log in to Spotify</h1>
             <a href="http://localhost:8888">
-            <button>Login</button>
+                <button>Login</button>
             </a>
+            <div>Now Playing {this.state.nowPlaying.name}</div>
+            <img src={this.state.nowPlaying.image} style={{ width: 100 }}/>
         </div>
-    )
+         )
+    }
+    
 }
 
 export default App
