40d5d134844f7a3d54a7e69fad94100cef1c1d04
Arianna Choza <unachoza@gmail.com>
2019-08-24 21:53:24 -0400

    working with spotify endpoints, can check users (hardcoded) playlists

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,29 +1,49 @@
 import React, {Component} from 'react'
 import '../App.css'
+import Spotify from 'spotify-web-api-js'
+
+const spotifyWebApi = new Spotify()
 
 class App extends Component {
     constructor() {
         super()
         const params = this.getHashParams()
+        const token = params.access_token
         this.state = {
-            loggedIn: params.access_token ? true : false,
+            loggedIn: token ? true : false,
             nowPlaying: {
-                name: "Not Checked", 
-                top: ''
+                name: "", 
             }
         }
+        if (token) {
+            console.log('got access token', token)
+            spotifyWebApi.setAccessToken(token)
+        }
+        console.log(this.state)
     }
     
 
-    getHashParams() {
-        console.log('something')
-          var hashParams = {};
-          var e, r = /([^&;=]+)=?([^&;]*)/g,
+   getHashParams() {
+          let hashParams = {};
+          let e, r = /([^&;=]+)=?([^&;]*)/g,
               q = window.location.hash.substring(1);
           while ( e = r.exec(q)) {
              hashParams[e[1]] = decodeURIComponent(e[2]);
           }
+       console.log('got hash params', hashParams)
           return hashParams;
+   }
+    
+    getNowPlaying() {
+        spotifyWebApi.getUserPlaylists("1224023576")
+            .then((response) => {
+            console.log("playing", response.items)
+            this.setState({
+                nowPlaying: {
+                    name: response.items[0].name,
+                }
+            })
+        })
     }
         
     render() {
@@ -33,8 +53,13 @@ class App extends Component {
                 <a href="http://localhost:8888">
                     <button>Login to Spotify</button>
                 </a>
-            <div>Now Playing {this.state.nowPlaying.name}</div>
-            <img src={this.state.nowPlaying.image} style={{ width: 100 }}/>
+                <div>
+                    {this.state.loggedIn &&
+                        <button onClick={() => this.getNowPlaying()}>Check What's Playing</button>}
+                </div>  
+                {this.state.nowPlaying.name? <div>These are All Your Playlists: {this.state.nowPlaying.name} </div>  : ""}
+            {/* <div>Your Playlists : {this.state.nowPlaying.name}</div> */}
+                 
         </div>
          )
     }
