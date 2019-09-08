0c30f68d7d09890829f127084879077be0582bf4
Arianna Choza <unachoza@gmail.com>
2019-09-08 11:37:40 -0400

    songs map songs map songs map

---
+++ client/src/Components/SongSingle.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../App.css'
+
+export const SongSingle = ({ track }) => {
+    return (
+        <div> 
+           {track}
+    </div>   
+            )
+    
+}
+
+
+export default SongSingle
