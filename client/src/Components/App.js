0434205c96da38c9cd86365de714869eafd01ec0
Arianna Choza <unachoza@gmail.com>
2019-09-10 19:45:06 -0400

    button checks single playlist, console logs if problem is present

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -15,13 +15,15 @@ class App extends Component {
             playlistNames: "",
             trackNamesArr: [],
             offsetNum: 0,
+            items: [],
             user: {
                 name: "", 
                 imageUrl: "",
                 id: ""
             }
         }
-        this.listTracksFromPlaylists = this.listTracksFromPlaylists.bind(this)
+        // this.listTracksFromPlaylists = this.listTracksFromPlaylists.bind(this)
+        // this.listTracksFromPlaylists() = this.listTracksFromPlaylists.bind(this)
         if (token) {
             spotifyWebApi.setAccessToken(token)
             this.getUserInfo()
@@ -63,10 +65,10 @@ class App extends Component {
     getPlaylists(){
         this.increaseOffset()
         // {limit: 50, offset: 0}
-        spotifyWebApi.getUserPlaylists(this.state.id, {limit: 50, offset: this.state.offsetNum})
+        spotifyWebApi.getUserPlaylists(this.state.id, {limit: 20, offset: this.state.offsetNum})
            
             .then((response) => {
-                console.log(response.items)
+                console.log(response.items.length)
                 this.setState({ playlistNames: response.items })
                 // console.log(this.state.playlistNames)
             })
@@ -74,50 +76,51 @@ class App extends Component {
     increaseOffset() {
         this.setState(state => {
             return {
-                offsetNum: state.offsetNum + 50
+                offsetNum: state.offsetNum + 20
             }
         })
         console.log(this.state)
     }
     //getting list of tracks in User's Playlists 
-    listTracksFromPlaylists(trackID) {
-        console.log('checking Songs in playlist')
-        spotifyWebApi.getPlaylistTracks(trackID)
-            .then((response ) => {
-                // console.log(response)
-                //saving variables of interested data points in response obj
-                let trackNames = []
-                let artistObjArr = []
-                let artistsNamesArr = []
-
-          response.items.map((item) => {
-            trackNames.push(item.track.name)
-        })
-        response.items.map(item => {
-            artistObjArr.push(item.track.artists)
-        })
-        artistObjArr.map((artist) => {
-            artistsNamesArr.push(artist[0].name)
-        })
-                let stones = artistsNamesArr.indexOf("The Rolling Stones")
-                // console.log(trackNames[rKellyVerdict] , "here")
-        if (stones > -1) {
-            console.log("Stones song here", trackNames[stones])
-        }
-            const chrisBrownVerdict = artistsNamesArr.indexOf("Chris Brown")
-        if (chrisBrownVerdict > -1 ) {
-            console.log("Chris Brown song here", trackNames[chrisBrownVerdict])
-        }
-        const rkelly = artistsNamesArr.indexOf("R. Kelly")
-        if (rkelly > -1) {
-            console.log("R. Kelly song here", trackNames[rkelly])
-        }
-// console.log(artistsNamesArr, trackNames, 'done')
-        return trackNames, artistObjArr, artistsNamesArr
+    // listTracksFromPlaylists(trackID) {
+    //     // console.log('checking Songs in playlist')
+    //     spotifyWebApi.getPlaylistTracks(trackID)
+    //         .then((response) => {
+    //             //saving variables of interested data points in response obj
+    //             let trackNames = []
+    //             let artistObjArr = []
+    //             let artistsNamesArr = []
+    //             let items = []
+              
                 
-            })
+    //       response.items.map((item) => {
+    //         trackNames.push(item.track.name)
+    //       })
         
-    }
+    //     response.items.map(item => {
+    //         artistObjArr.push(item.track.artists)
+    //     })
+    //     artistObjArr.map((artist) => {
+    //         artistsNamesArr.push(artist[0].name)
+    //     })
+    //             let mJSong = artistsNamesArr.indexOf("Michael Jackson")
+    //             // console.log(trackNames[rKellyVerdict] , "here")
+    //     if (mJSong > -1) {
+    //         console.log("mJSong song here", trackNames[mJSong])
+    //     }
+    //         const chrisBrownVerdict = artistsNamesArr.indexOf("Chris Brown")
+    //     if (chrisBrownVerdict > -1 ) {
+    //         console.log("Chris Brown song here", trackNames[chrisBrownVerdict])
+    //     }
+    //     const rkelly = artistsNamesArr.indexOf("R. Kelly")
+    //     if (rkelly > -1) {
+    //         console.log("R. Kelly song here", trackNames[rkelly])
+    //     }
+    //     return trackNames, artistObjArr, artistsNamesArr
+                
+    //         })
+        
+    // }
    
     findRKelly() {
     // rkelly id : "2mxe0TnaNL039ysAj51xPQ"
@@ -163,9 +166,11 @@ class App extends Component {
                     <PlaylistList
                     usersPlaylists={this.state.playlistNames}
                     tracksObject={this.state.trackNamesArr}
-                    trackList={this.listTracksFromPlaylists}
+                    // trackList={this.listTracksFromPlaylists()}
                     names={this.names}
+                    items={this.state.items}
                     />}
+                {/* {this.listTracksFromPlaylists("1ZmR4C1R0clb32v25PWzvD")} */}
             </div>
         )
     }
--- client/src/Components/PlaylistSingle.js
+++ client/src/Components/PlaylistSingle.js
@@ -1,23 +1,84 @@
-import React from 'react'
+import React, {Component} from 'react'
 import '../App.css'
 import Songs from './Songs'
+import Spotify from 'spotify-web-api-js'
 
-const PlaylistSingle = ({ playlistInfo, tracksObject, trackList, names }) => {
-    // console.log('hit playListSingle Componenet")
+const   spotifyWebApi = new Spotify()
+
+class PlaylistSingle extends Component {
+        constructor() {
+        super()
+        this.state = {
+            items: [],
+            rKellyVerdict: [],
+            chrisBrownVerdict: [],
+            trackNames: [],
+            artistsNamesArr: []
+            
+        }
+    }
+listTracksFromPlaylists(trackID) {
+        // console.log('checking Songs in playlist')
+        spotifyWebApi.getPlaylistTracks(trackID)
+            .then((response) => {
+                //saving variables of interested data points in response obj
+                let trackNames = []
+                let artistObjArr = []
+                let artistsNamesArr = []
+                let items = []
+              
+                
+          response.items.map((item) => {
+            trackNames.push(item.track.name)
+          })
+        
+        response.items.map(item => {
+            artistObjArr.push(item.track.artists)
+        })
+        artistObjArr.map((artist) => {
+            artistsNamesArr.push(artist[0].name)
+        })
+                let mJSong = artistsNamesArr.indexOf("Michael Jackson")
+                // console.log(trackNames[rKellyVerdict] , "here")
+        if (mJSong > -1) {
+            console.log("mJSong song here", trackNames[mJSong])
+        }
+            const chrisBrownVerdict = artistsNamesArr.indexOf("Chris Brown")
+        if (chrisBrownVerdict > -1 ) {
+            console.log("Chris Brown song here", trackNames[chrisBrownVerdict])
+        }
+        const rkelly = artistsNamesArr.indexOf("R. Kelly")
+        if (rkelly > -1) {
+            console.log("R. Kelly song here", trackNames[rkelly])
+        }
+        return trackNames[2]
+                
+            })
+    // return this.trackNames[2]
+        
+    }
+    render(){
+        // console.log('hit playListSingle Componenet')
+        // console.log(this.props)
     return (
         <div className="playlist-container"
             style={{ display: "inline-block", padding: "14px", border: "1px solid black", borderRadius: "4px", backgroundColor: "magenta", color: "white" }}>
             <div className="playlist-title">
-                {playlistInfo.name} 
-                {trackList(playlistInfo.id)}
+                Yes Mam'  {this.props.playlistInfo.name} 
+                <button onClick={(e) => this.listTracksFromPlaylists( this.props.playlistInfo.id)}>List Songs</button>
                 {/* <button onclick={renderSongs()}>Show Songs</button> */}
-                {names &&
-                    <Songs names={names} />
+                {this.props.names &&
+                    <Songs names={this.props.names} />
                 }
             </div>
         </div>      
     )
 }
+}
+// const PlaylistSingle = ({ playlistInfo, tracksObject, trackList, names }) => {
+    
+//     }
+  
 
 export default PlaylistSingle
 
