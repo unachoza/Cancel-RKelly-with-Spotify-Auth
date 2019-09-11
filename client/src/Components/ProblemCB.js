bf38fc0a9d92358040dc5a9da1435389e7d82cb6
Arianna Choza <unachoza@gmail.com>
2019-09-11 13:03:29 -0400

    conditional rendering for each artist problems

---
+++ client/src/Components/ProblemCB.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+import '../App.css'
+import Spotify from 'spotify-web-api-js'
+
+const spotifyWebApi = new Spotify()
+
+class ProblemCB extends Component{
+
+
+    // removeTracksFromPlaylist(playlistId, uris: Object[]) {
+
+    // }
+    // callback?:
+    // removeTracksFromPlaylist(playlistId: string, uris: Object[], callback?:
+    render() {
+        return (
+            <div style={{ color: "darkred", fontSize: "20px", fontWeight: "300" }}>
+                  <hr></hr>
+                <div>This is a problem : {this.props.chrisBrownVerdict}</div> 
+                <hr></hr>
+
+            </div>
+        )
+    }
+}
+export default ProblemCB
---
+++ client/src/Components/ProblemRK.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+import '../App.css'
+import Spotify from 'spotify-web-api-js'
+
+const spotifyWebApi = new Spotify()
+
+class ProblemRK extends Component{
+
+
+    // removeTracksFromPlaylist(playlistId, uris: Object[]) {
+
+    // }
+    // callback?:
+    // removeTracksFromPlaylist(playlistId: string, uris: Object[], callback?:
+    render() {
+        return (
+            <div style={{ color: "darkred", fontSize: "20px", fontWeight: "300" }}>
+                  <hr></hr>
+                <div>This is a problem : {this.props.rKellyVerdict}</div> 
+                <hr></hr>
+
+            </div>
+        )
+    }
+}
+
+
+
+
+export default ProblemRK
