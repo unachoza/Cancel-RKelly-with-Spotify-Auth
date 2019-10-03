c6de28348943c20758ed130a2cbc512177ab3f81
arianna choza <unachoza@gmail.com>
2019-10-03 17:13:18 -0400

    modified server

--- app.js
+++ app.js
@@ -145,3 +145,14 @@ app.get('/refresh_token', function(req, res) {
 
 console.log('Listening on 8888');
 app.listen(process.env.PORT || 5000)
+
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
