8e15edcee539d457333956433a56e4d7895040ea
arianna choza <unachoza@gmail.com>
2019-10-28 09:05:39 -0700

    added text regarding how it works, about me, and conclusion

---
+++ client/src/Components/AboutMe.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+const AboutMe = () => {
+    return (
+        <div>
+            <img src="" alt="img of app creator"/>
+            <p>
+               Arianna Choza is a FullStack Engineer who is driven to design a world that promotes equality by creating products that re-image existing conditions, pushing the future forward by build products that            inform users of their potential and empower them to realize it. 
+            </p>
+            <a href="www.linkedIn.com/in/arianna-choza">Find me on Linked In</a>
+            <a href="www.arianna-choza.surge.sh">Other Projects</a>
+            <a href="www.unachoza@gmail.com">Contact Me</a>
+        </div>
+    )
+}
+export default AboutMe
