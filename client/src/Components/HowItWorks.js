01545286785db9738480098a48a97aeabcc09f5d
arianna choza <unachoza@gmail.com>
2019-11-07 16:40:27 -0500

    fixing How it works typos

--- client/src/Components/HowItWorks.js
+++ client/src/Components/HowItWorks.js
@@ -15,10 +15,10 @@ const HowItWorks = () => {
                 Login with your Spotify credentials. This app will display your playlists and filter through each one to find any problematic / RKelly Songs. 
             </p>
             <p className="paragraph">
-                If any are present, you will be given the option to delete it. 
+                If any songs are present, you will be given the option to delete it. 
             </p>
             <p className="paragraph">
-                If the playlists with the problem is a public song you follow, A new playlist will be made without the song. You will be given the option to unfollow the problematic playlist and follow the problem free playlist. 
+                If the playlists with the problem is a public playlists, A new playlist will be made without the song. You will be given the option to unfollow the problematic playlist and follow the problem free playlist. 
             </p >
             <p className="paragraph">
                All songs found are only deleted once you have clicked the delete button. All public playlists are only unfollowed once you've clicked the unfollow button. 
