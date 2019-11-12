00869fd2ab2a7f7b5bfb1e3b18579594cec031a1
arianna choza <unachoza@gmail.com>
2019-11-12 13:21:26 -0500

    Added Carousel Annimation to How It Works, needs loads of styling; logic is solid

---
+++ client/src/Components/Carousel.js
@@ -0,0 +1,12 @@
+// import React, { Component } from 'react';
+
+ 
+// class TextCarousel extends Component {
+//     render() {
+//         return (
+            
+//         );
+//     }
+// }
+ 
+// export default TextCarousel
--- client/src/Components/HowItWorks.js
+++ client/src/Components/HowItWorks.js
@@ -1,35 +1,52 @@
-import React from 'react'
-import '../App.css'
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import "../App.css";
 
 const HowItWorks = () => {
 
-    const nextDescription = () => {
-        //animation to move text
-         
-    }
-
-    return (
+  return (
+    <div>
+      <Carousel showThumbs={false}>
+        >
         <div>
-            {/* <button className="right-arrow-description" onClick={nextDescription()}>How It Works</button> */}
-            <p className="paragraph">
-                Login with your Spotify credentials. This app will display your playlists and filter through each one to find any problematic / RKelly Songs. 
-            </p>
-            <p className="paragraph">
-                If any songs are present, you will be given the option to delete it. 
-            </p>
-            <p className="paragraph">
-                If the playlists with the problem is a public playlists, A new playlist will be made without the song. You will be given the option to unfollow the problematic playlist and follow the problem free playlist. 
-            </p >
-            <p className="paragraph">
-               All songs found are only deleted once you have clicked the delete button. All public playlists are only unfollowed once you've clicked the unfollow button. 
-            </p>
-            <p className="paragraph">
-                Removing his songs; remove his power; create consequence for abuse
-            </p>
-            <p>
-            </p>
+          <p className="paragraph">
+            Login with your Spotify credentials. This app will display your
+            playlists and filter through each one to find any problematic /
+            RKelly Songs.
+          </p>
         </div>
-    )
-}
+        <div>
+          <p className="paragraph">
+            If any songs are present, you will be given the option to delete it.
+          </p>
+        </div>
+        <div>
+          <p className="paragraph">
+            If the playlists with the problem is a public playlists, A new
+            playlist will be made without the song. You will be given the option
+            to unfollow the problematic playlist and follow the problem free
+            playlist.
+          </p>
+        </div>
+        <div>
+          <p className="paragraph">
+            All songs found are only deleted once you have clicked the delete
+            button. All public playlists are only unfollowed once you've clicked
+            the unfollow button.
+          </p>
+        </div>
+        <div>
+          <p className="paragraph">
+            Removing his songs; remove his power; create consequence for abuse
+          </p>
+        </div>
+      </Carousel>
+      {/* <button className="right-arrow-description" onClick={nextDescription()}>How It Works</button> */}
 
-export default HowItWorks
+      <p></p>
+    </div>
+  );
+};
+
+export default HowItWorks;
