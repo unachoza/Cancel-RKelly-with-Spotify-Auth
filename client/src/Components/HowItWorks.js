5fbb0edb894d853f50ae0cf51c655e99ccfabbf2
arianna choza <unachoza@gmail.com>
2019-11-12 14:05:04 -0500

    carousel styling better, but not first slide

--- client/src/Components/HowItWorks.js
+++ client/src/Components/HowItWorks.js
@@ -6,22 +6,23 @@ import "../App.css";
 const HowItWorks = () => {
 
   return (
-    <div>
-      <Carousel showThumbs={false}>
+    <div style={{backgroundColor: "rgb(29, 28, 28)",  margin: "0px" , width: "100%", height: "100%"}}>
+          <Carousel showThumbs={false} showStatus={false}
+           style={{backgroundColor: "rgb(29, 28, 28)", margin: "0px" ,width: "100%", height: "100%"}}>
         >
-        <div>
+        <div style={{backgroundColor: "rgb(29, 28, 28)",  margin: "0px" , width: "100%", height: "100%"}}>
           <p className="paragraph">
             Login with your Spotify credentials. This app will display your
             playlists and filter through each one to find any problematic /
             RKelly Songs.
           </p>
         </div>
-        <div>
+        <div  style={{backgroundColor: "rgb(29, 28, 28)",  margin: "0px" , width: "100%", height: "100%"}}>
           <p className="paragraph">
             If any songs are present, you will be given the option to delete it.
           </p>
         </div>
-        <div>
+        <div style={{backgroundColor: "rgb(29, 28, 28)",  margin: "0px" , width: "100%", height: "100%"}}>
           <p className="paragraph">
             If the playlists with the problem is a public playlists, A new
             playlist will be made without the song. You will be given the option
@@ -29,21 +30,20 @@ const HowItWorks = () => {
             playlist.
           </p>
         </div>
-        <div>
+        <div style={{backgroundColor: "rgb(29, 28, 28)",  margin: "0px", width: "100%", height: "100%"  }}>
           <p className="paragraph">
             All songs found are only deleted once you have clicked the delete
             button. All public playlists are only unfollowed once you've clicked
             the unfollow button.
           </p>
         </div>
-        <div>
+        <div style={{backgroundColor: "rgb(29, 28, 28)",  margin: "0px" , width: "100%", height: "100%"}}>
           <p className="paragraph">
-            Removing his songs; remove his power; create consequence for abuse
+            Removing his songs;  Remove his power;  Rreate consequence for abuse
           </p>
         </div>
       </Carousel>
-      {/* <button className="right-arrow-description" onClick={nextDescription()}>How It Works</button> */}
-
+      
       <p></p>
     </div>
   );
