830d36c0d13e3169c4d43e828acb17fd35aa44ad
arianna choza <unachoza@gmail.com>
2019-11-12 12:41:41 -0500

    entire Component did mount function works, checking all user playlists for problems and console's the playlist names

--- client/src/Components/MakingHash.js
+++ client/src/Components/MakingHash.js
@@ -4,116 +4,139 @@ import Spotify from "spotify-web-api-js";
 const spotifyWebApi = new Spotify();
 
 class MakingHash extends Component {
-  state = {
-    playlists: []
-  };
+    state = {
+        playlists: [],
+        problem : []
+    };
 
-  async componentDidMount() {
-    // const {user} = this.state
-    spotifyWebApi.getMe();
-    const userRes = await spotifyWebApi.getMe();
-    console.log(userRes);
-    this.setState({
-      id: userRes.id,
-      name: userRes.display_name,
-      email: userRes.email,
-      country: userRes.country,
-      totalPlaylists: userRes.total
-    });
-    console.log("this is stateu", this.state);
-    let offsetNum = -50;
-    let playlistResults = [];
-      let playlistPartialResults = [];
+    async componentDidMount() {
+        // const {user} = this.state
+        spotifyWebApi.getMe();
+        const userRes = await spotifyWebApi.getMe();
+        console.log(userRes);
+        this.setState({
+            id: userRes.id,
+            name: userRes.display_name,
+            email: userRes.email,
+            country: userRes.country,
+            totalPlaylists: userRes.total
+        });
+        console.log("this is stateu", this.state);
+        let offsetNum = -50;
+        let playlistResults = [];
+        let playlistPartialResults = [];
       
-    //geting playlists ids & names only 50
-    const loops = await spotifyWebApi.getUserPlaylists(this.state.id);
-    this.setState({ totalPlaylists: loops.total });
-    let loopsCount = Math.ceil(this.state.totalPlaylists / 50);
-    for (let i = 0; i < loopsCount; i++) {
-        offsetNum += 50;
+        //geting playlists ids & names only 50
+        const loops = await spotifyWebApi.getUserPlaylists(this.state.id);
+        this.setState({ totalPlaylists: loops.total });
+        let loopsCount = Math.ceil(this.state.totalPlaylists / 50);
+        for (let i = 0; i < loopsCount; i++) {
+            offsetNum += 50;
         
-      const temp = await spotifyWebApi.getUserPlaylists("8mcnehzy9pqb74j65vsfzhluu", {
-        limit: 50,
-        offset: offsetNum
-      })
-      playlistResults.push.apply(playlistResults, temp.items);
-      let playlistIds = [];
-        let playlistNames = [];
-        console.log(playlistResults)
-      playlistResults.map(index => {
-        playlistIds.push(index.id);
-        playlistNames.push(index.name);
-      });
+            const temp = await spotifyWebApi.getUserPlaylists("8mcnehzy9pqb74j65vsfzhluu", {
+                limit: 50,
+                offset: offsetNum
+            })
+            playlistResults.push.apply(playlistResults, temp.items);
+            let playlistIds = [];
+            let playlistNames = [];
+            console.log(playlistResults)
+            playlistResults.map(index => {
+                playlistIds.push(index.id);
+                playlistNames.push(index.name);
+            });
         
-        //State now has all PlayListIDS and their Names 
-      this.setState(prevState => ({
-        playlists: {
-          ...prevState.playlists,
-          playlistIds: [...playlistIds, playlistIds],
-          playlistNames: [...playlistNames, playlistNames]
+            //State now has all PlayListIDS and their Names 
+            this.setState(prevState => ({
+                playlists: {
+                    ...prevState.playlists,
+                    playlistIds: [...playlistIds, playlistIds],
+                    playlistNames: [...playlistNames, playlistNames]
+                }
+            }));
+            console.log("my state", this.state);
         }
-      }));
-      console.log("my state", this.state);
-    }
-      let count = 0
-      this.state.playlists.playlistIds.map( async (id)  =>  {
-          let tracks = await spotifyWebApi.getPlaylistTracks(id)
-          tracks = tracks.items
-          let artistsObj = []
-          for (let i = 0; i < tracks.length; i++){
-            artistsObj.push(tracks[i].track.artists)
-          } let artistsNames = []
-          console.log(artistsObj)
-          for (let i = 0; i < artistsObj.length; i++) {
-            artistsNames.push(artistsObj[i][0].name)
-          }
+        let count = 0
+        this.state.playlists.playlistIds.map(async (id) => {
+            let tracks = await spotifyWebApi.getPlaylistTracks(id)
+            tracks = tracks.items
+            let artistsObj = []
 
-          ///*****************looping and getting an array of all artist names for each playlist */
-          console.log(artistsNames)
-  
+            // getting the artist object, because the name is nested deeply
+            for (let i = 0; i < tracks.length; i++) {
+                artistsObj.push(tracks[i].track.artists)
+            }
+            let artistsNames = []
+            console.log(artistsObj)
+            for (let i = 0; i < artistsObj.length; i++) {
+                artistsNames.push(artistsObj[i][0].name)
+            }
+
+            ///*****************looping and getting an array of all artist names for each playlist */
+            console.log(artistsNames)
+            console.log(count)
+            let currentPlaylistName = this.state.playlists.playlistNames[count]
+            if (artistsNames.includes('R. Kelly')){
+                console.log('yes', currentPlaylistName)
+                this.setState(prevState => ({
+                    problem: [ ...prevState.problem, currentPlaylistName]
+                }))
+
+                // this.setState(prevState => ({
+                //     myArray: [...prevState.myArray, "new value"]
+                //   }))
+            } else {
+                console.log('no')
+            }
+             
+            console.log(this.state)
+            
+              count++
+            
+        }
+            
           
-      }) 
+            //   tracks = tracks[id].track.name
+            //   count ++
+            //   console.log()
+          
+            //   }) 
 
-      console.log(count)
-     let  currentPlaylistName = this.state.playlists.playlistNames[0]
-      console.log(currentPlaylistName)
-    const song =  await spotifyWebApi.getPlaylistTracks(
-        this.state.playlists.playlistIds.map((indexOfPlaylist) => {
-            let artistsFromOnePlaylist = []
+            //   console.log(count)
+            //  let  currentPlaylistName = this.state.playlists.playlistNames[0]
+            //   console.log(currentPlaylistName)
+            // const song =  await spotifyWebApi.getPlaylistTracks(
+            //     this.state.playlists.playlistIds.map((indexOfPlaylist) => {
+            //         let artistsFromOnePlaylist = []
 
-     song.items.map((index) => {
-         artistsFromOnePlaylist.push(index.track.artists[0].name)
-     })
-        })
+            //  song.items.map((index) => {
+            //      artistsFromOnePlaylist.push(index.track.artists[0].name)
+            //  })
+    
+        )
         
-        
-        [1]);
-      console.log(song.items);
-      let artistsFromOnePlaylist = []
+    }
 
-     song.items.map((index) => {
-         artistsFromOnePlaylist.push(index.track.artists[0].name)
-     })
+
+     
       
       //***********find rkelly and find out what playlist the song belongs to
       ///**********FINDS RKELLY AND PRINTS PLAYLISTNAME*/
       
-      if (artistsFromOnePlaylist.includes('R. Kelly')) {
-          console.log("The Playlist", currentPlaylistName, " has a Problem")
-          this.setState({
-              problem: currentPlaylistName
-          })
-          
-      }
-      
+  
       //from what playlist
-      console.log(artistsFromOnePlaylist)
-
-  }
+    //   console.log(artistsFromOnePlaylist)
     
 
   render() {
-    return <div>{`The Playlist, ${this.state.problem},  has a Problem`}</div>;
+      return (
+          <div>
+          {/* <div>{`The Playlist, ${this.state.problem[0]}  has a Problem`}</div>)
+          <div>{`The Playlist, ${this.state.problem[1]}  has a Problem`}</div>)
+          <div>{`The Playlist, ${this.state.problem[2]}  has a Problem`}</div>)
+     */}
+          </div>
+      )
   }
 }
 
