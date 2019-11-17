0b800a517236bc87bcd5ccc4a3d12dd6d69c9272
arianna choza <unachoza@gmail.com>
2019-11-17 01:23:11 -0500

    added carousel annimation back to how it works

--- client/src/Components/HowItWorks.js
+++ client/src/Components/HowItWorks.js
@@ -1,34 +1,44 @@
 import React from 'react'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 import '../App.css'
 
 const HowItWorks = () => {
 
-    const nextDescription = () => {
-        //animation to move text
-         
-    }
-
     return (
         <div>
-            {/* <button className="right-arrow-description" onClick={nextDescription()}>How It Works</button> */}
+        <Carousel showThumbs={false} showStatus={false}>
+          <div className="carousel-divs">
             <p className="paragraph">
-                Login with your Spotify credentials. This app will display your playlists and filter through each one to find any problematic / RKelly Songs. 
+              Login with your Spotify credentials. This app will display your
+              playlists and filter through each one to find any problematic /
+              RKelly Songs.
             </p>
+          </div>
+          <div className="carousel-divs">
             <p className="paragraph">
-                If any songs are present, you will be given the option to delete it. 
+              If any songs are present, you will be given the option to delete it.
             </p>
+          </div>
+          <div className="carousel-divs">
             <p className="paragraph">
-                If the playlists with the problem is a public playlists, A new playlist will be made without the song. You will be given the option to unfollow the problematic playlist and follow the problem free playlist. 
-            </p >
+              If the playlists with the problem is a public playlists, A new
+              playlist will be made without the song. You will be given the option to unfollow the problematic playlist and follow the problem free playlist.
+            </p>
+          </div>
+          <div className="carousel-divs">
             <p className="paragraph">
-               All songs found are only deleted once you have clicked the delete button. All public playlists are only unfollowed once you've clicked the unfollow button. 
+              All songs found are only deleted once you have clicked the delete button. All public playlists are only unfollowed once you've clicked the unfollow button.
             </p>
+          </div>
+          <div className="carousel-divs">
             <p className="paragraph">
-                Removing his songs; remove his power; create consequence for abuse
+              Removing his songs; Remove his power; Rreate consequence for abuse
             </p>
-            <p>
-            </p>
-        </div>
+          </div>
+        </Carousel>
+        <p></p>
+      </div>
     )
 }
 
--- client/src/Components/MakingHash.js
+++ client/src/Components/MakingHash.js
@@ -3,105 +3,91 @@ import Spotify from "spotify-web-api-js";
 const spotifyWebApi = new Spotify();
 
 class MakingHash extends Component {
-    state = {
-        playlists: [],
-        problem : []
-    };
+  state = {
+    playlists: [],
+    problem: []
+  };
 
-    async componentDidMount() {
-        // const {user} = this.state
-        const userRes = await spotifyWebApi.getMe();
-        this.setState({
-            id: userRes.id,
-            name: userRes.display_name,
-            email: userRes.email,
-            country: userRes.country,
-        });
-        let offsetNum = -50;
-        let playlistResults = [];
-      
-        //geting playlists ids & names only 50
-        const loops = await spotifyWebApi.getUserPlaylists(this.state.id);
-        this.setState({ totalPlaylists: loops.total });
-        let loopsCount = Math.ceil(this.state.totalPlaylists / 50);
-        //needs to loop if user has more than 50 playlists
-        for (let i = 0; i < loopsCount; i++) {
-            offsetNum += 50;
-        
-            const temp = await spotifyWebApi.getUserPlaylists(this.state.id, {limit: 50, offset: offsetNum })
-            playlistResults.push.apply(playlistResults, temp.items);
-            let playlistIds = [];
-            let playlistNames = [];
-            console.log(playlistResults)
-            playlistResults.map(index => {
-                playlistIds.push(index.id);
-                playlistNames.push(index.name);
-            });
-        
-            //State now has all PlayListIDS and their Names 
-            this.setState(prevState => ({
-                playlists: {
-                    ...prevState.playlists,
-                    playlistIds:  playlistIds,
-                    playlistNames:  playlistNames
-                }
-            }));
+  async componentDidMount() {
+    // const {user} = this.state
+    const userRes = await spotifyWebApi.getMe();
+    this.setState({
+      id: userRes.id,
+      name: userRes.display_name,
+      email: userRes.email,
+      country: userRes.country
+    });
+    let offsetNum = -50;
+    let playlistResults = [];
+
+    //geting playlists ids & names only 50
+    const loops = await spotifyWebApi.getUserPlaylists(this.state.id);
+    this.setState({ totalPlaylists: loops.total });
+    let loopsCount = Math.ceil(this.state.totalPlaylists / 50);
+    //needs to loop if user has more than 50 playlists
+    for (let i = 0; i < loopsCount; i++) {
+      offsetNum += 50;
+
+      const temp = await spotifyWebApi.getUserPlaylists(this.state.id, {
+        limit: 50,
+        offset: offsetNum
+      });
+      playlistResults.push.apply(playlistResults, temp.items);
+      let playlistIds = [];
+      let playlistNames = [];
+      console.log(playlistResults);
+      playlistResults.map(index => {
+        playlistIds.push(index.id);
+        playlistNames.push(index.name);
+      });
+
+      //State now has all PlayListIDS and their Names
+      this.setState(prevState => ({
+        playlists: {
+          ...prevState.playlists,
+          playlistIds: playlistIds,
+          playlistNames: playlistNames
         }
-        //getting the songs &aritist from each playlist
-        this.state.playlists.playlistIds.map(async id => {
-            let tracks = await spotifyWebApi.getPlaylistTracks(id)
-            tracks = tracks.items
-            let artistsObj = []
-
-            // getting the artist object, because the name is nested deeply
-            tracks.forEach(i => artistsObj.push(i.track.artists))
-            let artistsNames = []
-            artistsObj.forEach(obj => artistsNames.push(obj[0].name))
-
-           //checking if rkelly is in array of artists
-            if (artistsNames.includes('R. Kelly')) {
-                const res = await spotifyWebApi.getPlaylist(id)
-                console.log(res)
-                this.setState((prevState) => ({
-                    problem: prevState.problem.concat(res.name)
-                }))
-
-            } else {
-                console.log('no')
-            }
-             
-             console.log(this.state.problem)
-            
-        }
-            
-        )
-        
+      }));
     }
+    //getting the songs &aritist from each playlist
+    this.state.playlists.playlistIds.map(async id => {
+      let tracks = await spotifyWebApi.getPlaylistTracks(id);
+      tracks = tracks.items;
+      let artistsObj = [];
 
+      // getting the artist object, because the name is nested deeply
+      tracks.forEach(i => artistsObj.push(i.track.artists));
+      let artistsNames = [];
+      artistsObj.forEach(obj => artistsNames.push(obj[0].name));
 
-     
-      
-      //***********find rkelly and find out what playlist the song belongs to
-      ///**********FINDS RKELLY AND PRINTS PLAYLISTNAME*/
-      
-  
-      //from what playlist
-    //   console.log(artistsFromOnePlaylist)
-    
+      //checking if rkelly is in array of artists
+      if (artistsNames.includes("R. Kelly")) {
+        const res = await spotifyWebApi.getPlaylist(id);
+        //saving playlist name to state, if problem present
+        this.setState(prevState => ({
+          problem: prevState.problem.concat(res.name)
+        }));
+      }
+    });
+  }
+
+  //***********find rkelly and find out what playlist the song belongs to
+  ///**********FINDS RKELLY AND PRINTS PLAYLISTNAME*/
+
+ 
 
   render() {
-      return (
-          <div>
-            <div>{`The Playlist, ${this.state.problem[0]}  has a Problem`}</div>
-            <div>{`The Playlist, ${this.state.problem[1]}  has a Problem`}</div>
-            {/* <div>{`The Playlist, ${this.state.problem[2]}  has a Problem`}</div> */}
-    
-          </div>
-      )
+    return (
+      <div>
+        <div>{`The Playlist, ${this.state.problem[0]}  has a Problem`}</div>
+        <div>{`The Playlist, ${this.state.problem[1]}  has a Problem`}</div>
+        {/* <div>{`The Playlist, ${this.state.problem[2]}  has a Problem`}</div> */}
+      </div>
+    );
   }
 }
 
 export default MakingHash;
-
 
 // edge case: what if playlist has more than 100 songs
