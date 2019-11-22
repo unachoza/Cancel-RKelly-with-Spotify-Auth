56d7cba5a9eff2ed7cd15a86c3570c22cb0be3be
arianna choza <unachoza@gmail.com>
2019-11-21 19:56:18 -0500

    Nav Bar; shrunk logo and 80% pagewidth

--- client/src/Components/Nav.js
+++ client/src/Components/Nav.js
@@ -5,23 +5,23 @@ const Nav = ({ changeNav }) => {
         
     return (
       <div>
-        <div>
-          <img
+        
+
+        <div className="nav-container">
+       
+          <ul className="nav-list">
+            <li> <img
             className="spotify-logo"
             src="https://res.cloudinary.com/dh41vh9dx/image/upload/v1568208607/Spotify_Logo_CMYK_Green.png"
             alt="spotify logo"
-          />
-        </div>
-
-        <div className="nav-container">
-          <ul className="nav-list">
+          /></li>
             <li id="home" onClick={(e) => changeNav(e)}>HOME</li>
             <li id="login" onClick={(e) => changeNav(e)}>VIEW YOUR PLAYLISTS</li>
             <li id="howItWorks" onClick={(e) => changeNav(e)}>HOW IT WORKS</li>
             <li id="aboutMe" onClick={(e) => changeNav(e)}>ABOUT ME</li>
           </ul>
+          </div>
         </div>
-      </div>
     );
   }
 
